58368dcdd69ec845bb27dedcf939b2f5e9cd9491
Ronaldjga <Ronaldalmeida20022003@gmail.com>
2022-03-23 17:48:09 -0300

    ta funcionando
    com dois clickes

--- src/components/pokemons.js
+++ src/components/pokemons.js
@@ -4,38 +4,28 @@ import { Table } from "./table";
 
 export function PokemonsBanner() {
     const [pokemonData, setPokemonData] = react.useState([])
-    const [pokemon, setPokemon] = react.useState('')
     const [pokemonType, setPokemonType] = react.useState('')
-    const toArray = []
-    
-    
-    // const randomPokemon = async () => {
-    //     const randomPokemon = Math.floor(Math.random() * 300);
-    //     try {
-    //         const url = `https://pokeapi.co/api/v2/pokemon?limit=300&offset=200`
-    //         await axios.get(url)
-    //             .then(res => toArray.push(res.data.results))
-    //         setPokemonData(...toArray)
-    //     }
-    //     catch (e) {
-    //         console.log(e)
-    //     }
-    // }
+    const [keyIndex, setKeyIndex] = react.useState(0)
+
+    const [randomPokemon, setRandomPokemon] = react.useState([])
+    const [whatPokemon, setWhatPokemon] = react.useState('')
+
 
     react.useEffect(() => {
-        randomPokemon()
+        getRandomPokemon()
     },[])
 
-    const randomPokemon = async () => {
-        const randomPokemon = Math.floor(Math.random() * 300);
+
+
+    const getRandomPokemon = async () => {
+        const randomIndice = Math.floor(Math.random() * 300);
+        const toArray = []
         try {
             const url = `https://pokeapi.co/api/v2/pokemon?limit=300&offset=200`
             const res = await axios.get(url)
             toArray.push(res.data.results)
-            setPokemonData(...toArray)
-            if (pokemonData.length >= 1) {
-                 setPokemon(pokemonData[randomPokemon].name)
-             }
+            setRandomPokemon(...toArray)
+            setWhatPokemon(randomPokemon[randomIndice].name)
             // console.log(res)
         }
         catch (e) {
@@ -43,62 +33,49 @@ export function PokemonsBanner() {
         }
     }
 
-    return (
-        <section className="w-full h-screen bg-red-400">
-            <div className="max-w-screen-xl mx-auto px-2 py-5">
-                <button
-                    className=" bg-yellowPrimary text-darkBluePrimary font-bold py-2 px-2"
-                    onClick={(e) => {
-                        e.preventDefault
-                        randomPokemon()
-                        // console.log(pokemonData)
-                        // console.log(pokemon)
-                        // console.log(toArray)
-                    }}
-                >
-                    gera pokemon Aleatorio
-                </button>
-                {console.log(pokemon)}
-                
-                {/* <PokeBanner
-                   
-                /> */}
-                
-            </div>
-        </section>
-    )
-}
-
-function PokeBanner(props) {
-    const [pokemonSeach, setPokemonSeach] = react.useState('')
-    const [pokemonStats, setPokemonStats] = react.useState([])
-    const [pokemonType, setPokemonType] = react.useState('')
-    const [keyIndex, setKeyIndex] = react.useState(0)
-
-
-
-    const getPokemon = async () => {
+    const getPokemon = async (e) => {
         const toArray = [];
         try {
-            const url = `https://pokeapi.co/api/v2/pokemon/${pokemonSeach}`
+            const url = `https://pokeapi.co/api/v2/pokemon/${e}`
             const res = await axios.get(url)
             toArray.push(res.data);
             setPokemonType(res.data.types[0].type.name);
-            setPokemonStats(toArray);
+            setPokemonData(toArray);
             console.log(res)
-        } catch (e) {
+        }
+        catch (e) {
             console.log(e)
         }
     }
 
+    const handleSubmit = (e) => {
+        e.preventDefault()
+        setKeyIndex(keyIndex + 1)
+        console.log(randomPokemon, 'aaaaaaaa')
+        console.log(whatPokemon, 'aaaaaaaa')
+        getRandomPokemon()
+        getPokemon(whatPokemon)
+    }
+
     return (
-        <div>
-            {console.log}
-            {pokemonStats.map((data) => {
+        <section className="w-full h-screen bg-red-400">
+            <div className="max-w-screen-xl mx-auto px-2 py-5">
+            <form
+                    onSubmit={handleSubmit}
+                    className="w-full flex justify-center p-2 gap-5"
+                >
+                    <button
+                        onSubmit={handleSubmit}
+                        className="w-2/4 py-2 px-5 bg-yellowPrimary text-darkBluePrimary font-bold"
+                    >
+                        Enviar
+                    </button>
+                </form>
+                {pokemonData.map((data) => {
                     return (
                         <div
                             key={keyIndex}
-                            className="w-3/4 flex mx-auto bg-gray-900 relative">
+                            className="w-3/4 flex mx-auto bg-gray-900 relative text-white">
                             <img
                                 className="w-full z-10"
                                 key={data.id}
@@ -113,6 +90,7 @@ function PokeBanner(props) {
                         </div>
                     )
                 })}
-        </div>
+            </div>
+        </section>
     )
 }
