a764a9189a2863793e824a8360882a05054aa3a7
Ronaldjga <Ronaldalmeida20022003@gmail.com>
2022-03-24 18:24:48 -0300

    new commit

---
+++ pages/ghostType.js
@@ -0,0 +1,17 @@
+import react, { useState } from "react";
+import { useEffect } from "react/cjs/react.production.min";
+import Image from "next/image";
+import { PokemonTypes } from "../src/components/firePokemon";
+
+
+
+export default function FireType() {
+    return (
+        <div className="bg-darkBluePrimary flex flex-col gap-5 h-fit min-h-full">
+            <PokemonTypes
+                type='ghost'
+                indiceType='0'
+            />
+        </div>
+    )
+}
