0d3334366b5ab7a93c25eb7ae02347c0f5cdcc0e
Ronaldjga <Ronaldalmeida20022003@gmail.com>
2022-03-24 14:49:57 -0300

    peguei os nomes
    peguei os nomes de todos individualmente e coloquei em pokemons

--- src/components/firePokemon.js
+++ src/components/firePokemon.js
@@ -14,10 +14,22 @@ export function FireTypes() {
     const getPokemons = () => {
         axios.get(`https://pokeapi.co/api/v2/type/fire`)
             .then(res => {
-                toArray.push(res.data.pokemon.pokemon.name)
-                toArray.forEach(function(nome, i) {
-                    console.log('[forEach]', nome, i);
+                toArray.push(res.data.pokemon)
+                toArray.forEach(function (nome, i) {
+                    
+                    nome.map((a, e) => {
+                        setPokemons(a.pokemon.name)
+                        
+                    });
                 })
+
+
+
+
+
+
+
+                
                 //console.log(toArray)
             })
             .catch(e => {
@@ -28,29 +40,32 @@ export function FireTypes() {
 
 
     return (
-        <section className="w-full">
-            <div className="max-w-screen-xl mx-auto px-2 py-5">
-                {pokemons.map((data) => {
-                    return (
-                        <div
-                            key={keyIndex}
-                            className="w-3/4 flex flex-col sm:flex-row justify-center items-center p-2 mx-auto bg-gray-900 relative text-white">
-                            <img
-                                className="w-full z-10"
-                                key={data.id}
-                                src={data.sprites.front_default}
-                            />
-                            <TableRandom
-                                pokemonName={data.name}
-                                pokemonType={pokemonType}
-                                height={`${Math.round(data.height * 10)} cm`}
-                                weight={`${Math.round(data.weight / 10)} kg`}
-                                baseExp={data.base_experience}
-                            />
-                        </div>
-                    )
-                })}
-            </div>
-        </section>
+        <>
+            {console.log(pokemons)}
+        </>
+        // <section className="w-full">
+        //     <div className="max-w-screen-xl mx-auto px-2 py-5">
+        //         {pokemons.map((data) => {
+        //             return (
+        //                 <div
+        //                     key={keyIndex}
+        //                     className="w-3/4 flex flex-col sm:flex-row justify-center items-center p-2 mx-auto bg-gray-900 relative text-white">
+        //                     <img
+        //                         className="w-full z-10"
+        //                         key={data.id}
+        //                         src={data.sprites.front_default}
+        //                     />
+        //                     <TableRandom
+        //                         pokemonName={data.name}
+        //                         pokemonType={pokemonType}
+        //                         height={`${Math.round(data.height * 10)} cm`}
+        //                         weight={`${Math.round(data.weight / 10)} kg`}
+        //                         baseExp={data.base_experience}
+        //                     />
+        //                 </div>
+        //             )
+        //         })}
+        //     </div>
+        // </section>
     )
 }
