37f8f7e3e68a1a9436a00cf3c4af5ba30cb2d182
Ronaldjga <Ronaldalmeida20022003@gmail.com>
2022-03-24 17:41:57 -0300

    Pokemontypes
    complete

--- src/components/firePokemon.js
+++ src/components/firePokemon.js
@@ -8,74 +8,75 @@ export function FireTypes() {
     const [pokemonType, setPokemonType] = react.useState('')
     
     const [pokemonData, setPokemonData] = react.useState([])
+    const [start, setStart] = react.useState(0)
     
     const toArray = []
     const pegaNome = []
+    const allPokemons = [pokemonData]
 
     react.useEffect(() => {
 
         getPokemons()
     }, [])
 
-    const getPokemons = () => {
-        axios.get(`https://pokeapi.co/api/v2/type/fire`)
-            .then(res => {
-                toArray.push(res.data.pokemon)
-                toArray.forEach((pokeObj, i) => {
-                    pokeObj.map((url, i) => {
-                        const toArrayGetPokemon = [];
-                        axios.get(url.pokemon.url)
-                            .then(res => {
-                                toArrayGetPokemon.push(res.data);
-                                setPokemonType(res.data.types[0].type.name);
-                                setPokemonData(toArrayGetPokemon);
-                                console.log(res)
-                            })
-                            .catch(e => {
-                                console.log(e)
-                            })
-                        //console.log(url.pokemon.url) url de cada um
+    const getPokemons = async () => {
+        if (start === 0) {
+            setStart(2)
+            await axios.get(`https://pokeapi.co/api/v2/type/fire`)
+                .then(res => {
+                    toArray.push(res.data.pokemon)
+                    toArray.forEach((pokeObj, i) => {
+                        pokeObj.forEach((url, i) => {
+                            const toArrayGetPokemon = [];
+                            axios.get(url.pokemon.url)
+                                .then(res => {
+                                    pokemonData.push(res.data);
+                                    setPokemonType(res.data.types[0].type.name);
+                                    //console.log(toArrayGetPokemon)
+                                    //console.log(res)
+                                })
+                                .catch(e => {
+                                    console.log(e)
+                                })
+                            //console.log(url.pokemon.url) url de cada um
+                        })
                     })
+                
+                })
+                .catch(e => {
+                    console.log(e)
                 })
             
-            })
-            .catch(e => {
-                console.log(e)
-            })
+        } else return
     }
 
-    const getOnePokemon = () => {
-        const toArrayGetPokemon = [];
-        axios.get(`https://pokeapi.co/api/v2/pokemon/ditto`)
-            .then(res => {
-                toArrayGetPokemon.push(res.data);
-                setPokemonType(res.data.types[0].type.name);
-                setPokemonData(toArrayGetPokemon);
-                console.log(res)
-            })
-            .catch(e => {
-            console.log(e)
-            })
+    // const getOnePokemon = () => {
+    //     const toArrayGetPokemon = [];
+    //     axios.get(`https://pokeapi.co/api/v2/pokemon/ditto`)
+    //         .then(res => {
+    //             toArrayGetPokemon.push(res.data);
+    //             setPokemonType(res.data.types[0].type.name);
+    //             setPokemonData(toArrayGetPokemon);
+    //             console.log(res)
+    //         })
+    //         .catch(e => {
+    //         console.log(e)
+    //         })
         
                 
             
                 
-    }
+    // }
 
 
 
     return (
         <section className="w-full">
             <div className="max-w-screen-xl mx-auto px-2 py-5">
-                {}
                 <div
-                    className="grid grid-cols-3"
+                    className="grid grid-cols-3 gap-3"
                 >
-                    {/* {pokemonData.forEach((nomePokemon, indice) => {
-                        const PegaPfv = nomePokemon
-                        return (
-                            <div>
-                                {pokemonData.map((data) => {
+                    {pokemonData.map((data) => {
                                     return (
                                         <div
                                             key={data.id}
@@ -95,10 +96,6 @@ export function FireTypes() {
                                         </div>
                                     )
                                 })}
-                            </div>
-                        )    
-                        
-                    })} */}
                 </div>
             </div>
         </section>
