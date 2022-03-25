05223051ce2ea0e3fb8d7a1710f0535ee167342a
Ronaldjga <Ronaldalmeida20022003@gmail.com>
2022-03-25 17:29:38 -0300

    Update report.js

--- pages/report.js
+++ pages/report.js
@@ -5,7 +5,7 @@ import { BackHome, FormReport } from "../src/components/formReport";
 export default function Report() {
     return (
         <>
-            <div className="w-full h-fit bg-darkBluePrimary flex flex-col items-center md:justify-center pt-16 gap-5">
+            <div className="w-full h-fit bg-darkBluePrimary flex flex-col items-center md:justify-center px-4 pt-16 gap-5">
                 <h1 className="text-white font-h1Title text-center text-5xl font-bold p-5">Reporte</h1>
                 <p className="max-w-screen-lg mx-auto text-white text-center">
                     Esse site sobre pokemon foi desenvolvivo exclusivamente para reforçar conhecimentos e habilidades de desenvolvimento web, sendo assim, não tem fins lucrativos. Entretanto, você encontrou um erro? Reporte para que haja correção.
