1167853d662e955e00934b8275d694150af69598
Ronaldjga <Ronaldalmeida20022003@gmail.com>
2022-03-25 15:43:39 -0300

    introduction

--- pages/index.js
+++ pages/index.js
@@ -5,12 +5,14 @@ import Image from "next/image";
 import { FormPokemon } from "../src/components/form";
 import { PokemonsBanner } from "../src/components/pokemons";
 import { ButtonNav, NavHeader } from "../src/components/buttonNav";
+import { Introduction } from "../src/components/introduction";
 
 
 export default function HomePage() {
     return (
         <div className="bg-darkBluePrimary flex flex-col gap-5 h-fit min-h-full">
-            <NavHeader/>
+            <NavHeader />
+            <Introduction/>
             <FormPokemon />
             <PokemonsBanner />
         </div>
---
+++ src/components/introduction.js
@@ -0,0 +1,27 @@
+import react from "react";
+import Image from "next/dist/client/image";
+import pikachu from './img/pikachu.svg'
+import pokemonLogo from './img/pokemonLogo.svg'
+
+export function Introduction() {
+    return (
+        <main className="w-full max-w-screen-xl mx-auto flex flex-col justify-center items-center py-10 gap-5">
+            <div className="w-[200px] md:hidden relative">
+                <Image
+                    layout="responsive"
+                    src={pokemonLogo}
+                />
+            </div>
+            <div className="w-[200px] md:w-[300px] relative">
+                <Image
+                    layout="responsive"
+                    src={pikachu}
+                />
+            </div>
+            <p
+                className="text-white text-center md:w-3/4 text-xl">
+                Este site e desenvolvido exclusivamente para pratica de habilidade e tecnologias de desenvolvimento web. Sendo assim não possuimos vinculo com a nitendo, ou seja, não possuimos fins lucrativos.
+            </p>
+        </main>
+    )
+}
